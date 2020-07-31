c4dc90e039670a3081043506c0a9c83212d6850c
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2020-07-31 17:43:54 +0200

    Add a 'Coverage' build type when calling handle_default_build_type()

--- utility/build_type_handler.cmake
+++ utility/build_type_handler.cmake
@@ -18,6 +18,8 @@ available for the CMake GUI/TUI application.
 function(handle_default_build_type)
     # only check against CMAKE_BUILD_TYPE if generation is not for an IDE
     if(NOT CMAKE_CONFIGURATION_TYPES)
+        define_coverage_build_type_variables()
+
         # define default build type variable
         set(default_build_type "Debug")
 
@@ -37,12 +39,12 @@ function(handle_default_build_type)
                 CACHE
                     CMAKE_BUILD_TYPE
                 PROPERTY
-                    STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo"
+                    STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo" "Coverage"
             )
         endif()
 
         # check if the build type is valid
-        set(valid_build_types "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+        set(valid_build_types "Debug" "Release" "MinSizeRel" "RelWithDebInfo" "Coverage")
 
         if(NOT CMAKE_BUILD_TYPE IN_LIST valid_build_types)
             string(REPLACE ";" " / " info_build_types "${valid_build_types}")
@@ -54,4 +56,50 @@ function(handle_default_build_type)
     else()
         message(VERBOSE "Build type handling does not have an effect for IDE generators!")
     endif()
+endfunction()
+
+#[[
+
+-- INTERNAL FUNCTION
+
+Defines global CMake variables for the Coverage build type.
+
+define_coverage_build_type_variables()
+
+TODO:
+- add option to use different flags for Clang compiler
+  (https://clang.llvm.org/docs/SourceBasedCodeCoverage.html)
+- handle compiler which do not support coverage e.g. MSVC
+#]]
+function(define_coverage_build_type_variables)
+    set(compiler_flags "-g -O0 -fprofile-arcs -ftest-coverage")
+    set(linker_flags "--coverage")
+
+    set(CMAKE_C_FLAGS_COVERAGE "${compiler_flags}"
+        CACHE
+            STRING "Flags used by the C compiler during coverage builds."
+        FORCE
+    )
+    set(CMAKE_CXX_FLAGS_COVERAGE "${compiler_flags}"
+        CACHE
+            STRING "Flags used by the C++ compiler during coverage builds."
+        FORCE
+    )
+    set(CMAKE_EXE_LINKER_FLAGS_COVERAGE "${linker_flags}"
+        CACHE
+            STRING "Flags used for linking binaries during coverage builds."
+        FORCE
+    )
+    set(CMAKE_SHARED_LINKER_FLAGS_COVERAGE "${linker_flags}"
+        CACHE
+            STRING "Flags used by the shared libraries linker during coverage builds."
+        FORCE
+    )
+
+    mark_as_advanced(
+        CMAKE_C_FLAGS_COVERAGE
+        CMAKE_CXX_FLAGS_COVERAGE
+        CMAKE_EXE_LINKER_FLAGS_COVERAGE
+        CMAKE_SHARED_LINKER_FLAGS_COVERAGE
+    )
 endfunction()

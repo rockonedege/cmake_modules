5bb4107167fef443fee874133155c30ee85c7959
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2020-08-01 12:42:08 +0200

    Add comment to each compiler flags to describe the meaning

--- compiler/common_compiler_flags.cmake
+++ compiler/common_compiler_flags.cmake
@@ -30,44 +30,44 @@ function(get_common_cxx_compiler_flags output_var)
     if(("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU") OR ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang"))
         # list of compiler flags which should be used
         list(APPEND compiler_flags
-            -pedantic
-            -pedantic-errors
-            -Wextra
-            -Wall
-            -Wdouble-promotion
-            -Wundef
-            -Wshadow
-            -Wnull-dereference
-            -Wzero-as-null-pointer-constant
-            -Wunused
-            -Wold-style-cast
-            -Wsign-compare
-            -Wunreachable-code
-            -Wunreachable-code-break
-            -Wunreachable-code-return
-            -Wextra-semi-stmt
-            -Wreorder
-            -Wcast-qual
-            -Wconversion
-            -Wfour-char-constants
-            -Wformat=2
-            -Wheader-hygiene
-            -Wnewline-eof
-            -Wnon-virtual-dtor
-            -Wpointer-arith
-            -Wfloat-equal
-            -Wpragmas
-            -Wreserved-user-defined-literal
-            -Wsuper-class-method-mismatch
-            -Wswitch-enum
-            -Wcovered-switch-default
-            -Wthread-safety
-            -Wunused-exception-parameter
-            -Wvector-conversion
-            -Wkeyword-macro
-            -Wformat-pedantic
-            -Woverlength-strings
-            -Wdocumentation
+            -pedantic # collection of multiple flags
+            -pedantic-errors # turn pedantic warnings into errors
+            -Wextra # collection of multiple flags
+            -Wall # collection of multiple flags
+            -Wdouble-promotion # implicit conversion increases floating-point precision
+            -Wundef # X is not defined, evaluates to 0
+            -Wshadow # declaration shadows something
+            -Wnull-dereference # binding dereferenced null pointer to reference has UB
+            -Wzero-as-null-pointer-constant # zero as null pointer constant
+            -Wunused # warn on anything being unused
+            -Wold-style-cast # use of old-style cast
+            -Wsign-compare # comparison of integers of different signs
+            -Wunreachable-code # code will never be executed
+            -Wunreachable-code-break # 'break' will never be executed
+            -Wunreachable-code-return # 'return' will never be executed
+            -Wextra-semi-stmt # empty expression statement has no effect
+            -Wreorder # warn on anything that will be initialized after use
+            -Wcast-qual # cast drops qualifiers
+            -Wconversion # implicit conversion
+            -Wfour-char-constants # multi-character character constant
+            -Wformat=2 # warn on security issues around functions that format output
+            -Wheader-hygiene # using namespace directive in global context in header
+            -Wnewline-eof # no newline at end of file
+            -Wnon-virtual-dtor # X has virtual functions but non-virtual destructor
+            -Wpointer-arith # warn of problems with pointer arithmetic
+            -Wfloat-equal # comparing floating point with == or != is unsafe
+            -Wpragmas # warn on wrong #pragma usage
+            -Wreserved-user-defined-literal # invalid suffix on literal
+            -Wsuper-class-method-mismatch # method parameter type does not match super class
+            -Wswitch-enum # warn when enumeration values are not handles in switch
+            -Wcovered-switch-default # default label in switch which covers all enumeration values
+            -Wthread-safety # warn on anything that breaks thread safety
+            -Wunused-exception-parameter # unused exception parameter
+            -Wvector-conversion # incompatible vector types
+            -Wkeyword-macro # keyword is hidden by macro definition
+            -Wformat-pedantic # format type mismatch
+            -Woverlength-strings # string literal exceeds maximum length
+            -Wdocumentation # warn on documentation mismatches and related problems
         )
 
         if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
@@ -82,13 +82,13 @@ function(get_common_cxx_compiler_flags output_var)
             # things the flag could be used but on the real build all flags are used
             # and 'GCC' would throw an error.
             list(APPEND compiler_flags
-                -Wno-gnu-zero-variadic-macro-arguments
+                -Wno-gnu-zero-variadic-macro-arguments # must specify at least one argument
             )
         endif()
 
         if(${PROJECT_NAME}_compiler_warnings_as_errors)
             list(APPEND compiler_flags
-                -Werror
+                -Werror # turn all warnings into errors
             )
         endif()
     else()
@@ -123,36 +123,36 @@ function(get_common_c_compiler_flags output_var)
     if(("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU") OR ("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang"))
         # list of compiler flags which should be used
         list(APPEND compiler_flags
-            -pedantic
-            -pedantic-errors
-            -Wextra
-            -Wall
-            -Wdouble-promotion
-            -Wundef
-            -Wshadow
-            -Wnull-dereference
-            -Wunused
-            -Wsign-compare
-            -Wunreachable-code
-            -Wunreachable-code-break
-            -Wunreachable-code-return
-            -Wextra-semi-stmt
-            -Wreorder
-            -Wcast-qual
-            -Wconversion
-            -Wfour-char-constants
-            -Wformat=2
-            -Wheader-hygiene
-            -Wnewline-eof
-            -Wpointer-arith
-            -Wfloat-equal
-            -Wpragmas
-            -Wswitch-enum
-            -Wcovered-switch-default
-            -Wthread-safety
-            -Wkeyword-macro
-            -Wformat-pedantic
-            -Wdocumentation
+            -pedantic # collection of multiple flags
+            -pedantic-errors # turn pedantic warnings into errors
+            -Wextra # collection of multiple flags
+            -Wall # collection of multiple flags
+            -Wdouble-promotion # implicit conversion increases floating-point precision
+            -Wundef # X is not defined, evaluates to 0
+            -Wshadow # declaration shadows something
+            -Wnull-dereference # binding dereferenced null pointer to reference has UB
+            -Wunused # warn on anything being unused
+            -Wsign-compare # comparison of integers of different signs
+            -Wunreachable-code # code will never be executed
+            -Wunreachable-code-break # 'break' will never be executed
+            -Wunreachable-code-return # 'return' will never be executed
+            -Wextra-semi-stmt # empty expression statement has no effect
+            -Wreorder # warn on anything that will be initialized after use
+            -Wcast-qual # cast drops qualifiers
+            -Wconversion # implicit conversion
+            -Wfour-char-constants # multi-character character constant
+            -Wformat=2 # warn on security issues around functions that format output
+            -Wheader-hygiene # using namespace directive in global context in header
+            -Wnewline-eof # no newline at end of file
+            -Wpointer-arith # warn of problems with pointer arithmetic
+            -Wfloat-equal # comparing floating point with == or != is unsafe
+            -Wpragmas # warn on wrong #pragma usage
+            -Wswitch-enum # warn when enumeration values are not handles in switch
+            -Wcovered-switch-default # default label in switch which covers all enumeration values
+            -Wthread-safety # warn on anything that breaks thread safety
+            -Wkeyword-macro # keyword is hidden by macro definition
+            -Wformat-pedantic # format type mismatch
+            -Wdocumentation # warn on documentation mismatches and related problems
         )
 
         if("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang")
@@ -167,13 +167,13 @@ function(get_common_c_compiler_flags output_var)
             # things the flag could be used but on the real build all flags are used
             # and 'GCC' would throw an error.
             list(APPEND compiler_flags
-                -Wno-gnu-zero-variadic-macro-arguments
+                -Wno-gnu-zero-variadic-macro-arguments # must specify at least one argument
             )
         endif()
 
         if(${PROJECT_NAME}_compiler_warnings_as_errors)
             list(APPEND compiler_flags
-                -Werror
+                -Werror # turn all warnings into errors
             )
         endif()
     else()

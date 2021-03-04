8199d1139fab9071e10bd12e46648e602bf57fd3
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2021-03-04 13:57:16 +0100

    Improve the target configuration function to work with INTERFACE targets

--- utility/target_configuration.cmake
+++ utility/target_configuration.cmake
@@ -13,12 +13,16 @@ configure_target(TARGET <target>
                  [COMPILER_FLAGS <flag>...]
                  [SANITIZER_FLAGS <flag>...]
                  [LINKER_FLAGS <flag>...]
-                 [PUBLIC_DEFINITION_FLAGS <flag>...]
-                 [PRIVATE_DEFINITION_FLAGS <flag>...]
-                 [PUBLIC_INCLUDE_DIRS <dir>...]
-                 [PRIVATE_INCLUDE_DIRS <dir>...]
+                 [<scope>_DEFINITION_FLAGS <flag>...]
+                 [<scope>_INCLUDE_DIRS <dir>...]
                  [BUILD_TYPE_AS_OUTPUT_DIR]
+                 [ENABLE_UNUSED_SECTION_GARBAGE_COLLECTION]
 )
+
+The available values for all arguments with '<scope>' are:
+    - PUBLIC
+    - INTERFACE
+    - PRIVATE
 
 - TARGET
 Target which should be configured.
@@ -45,11 +49,17 @@ List of definition flags. Will be added to the PUBLIC scope.
 - PRIVATE_DEFINITION_FLAGS
 List of definition flags. Will be added to the PRIVATE scope.
 
+- INTERFACE_DEFINITION_FLAGS
+List of definition flags. Will be added to the INTERFACE scope.
+
 - PUBLIC_INCLUDE_DIRS
 List of include directories. Will be added to the PUBLIC scope.
 
 - PRIVATE_INCLUDE_DIRS
 List of include directories. Will be added to the PRIVATE scope.
+
+- INTERFACE_INCLUDE_DIRS
+List of include directories. Will be added to the INTERFACE scope.
 
 - BUILD_TYPE_AS_OUTPUT_DIR
 Option which changes the build output directory and uses a sub folder with the build type.
@@ -85,8 +95,10 @@ function(configure_target)
         LINKER_FLAGS
         PUBLIC_DEFINITION_FLAGS
         PRIVATE_DEFINITION_FLAGS
+        INTERFACE_DEFINITION_FLAGS
         PUBLIC_INCLUDE_DIRS
         PRIVATE_INCLUDE_DIRS
+        INTERFACE_INCLUDE_DIRS
     )
 
     # use cmake helper function to parse passed arguments
@@ -103,28 +115,73 @@ function(configure_target)
         message(FATAL_ERROR "TARGET argument required!")
     endif()
 
+    # get the type of the target as INTERFACE libraries should be handled differently
+    # -> only the INTERFACE scope properties can be set
+    get_target_property(${tpre_TARGET}_type ${tpre_TARGET} TYPE)
+    set(interface_target "FALSE")
+    if(${tpre_TARGET}_type MATCHES "INTERFACE_LIBRARY")
+        set(interface_target "TRUE")
+    endif()
+
+
     # set compile flags
-    target_compile_options(${tpre_TARGET}
-        PRIVATE
-            ${tpre_COMPILER_FLAGS}
-            $<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>,$<CONFIG:Coverage>>:${tpre_SANITIZER_FLAGS}>
-    )
+    if(DEFINED tpre_COMPILER_FLAGS AND (NOT ${interface_target}))
+        target_compile_options(${tpre_TARGET}
+            PRIVATE
+                ${tpre_COMPILER_FLAGS}
+        )
+    endif()
+
+    if(DEFINED tpre_SANITIZER_FLAGS AND (NOT ${interface_target}))
+        target_compile_options(${tpre_TARGET}
+            PRIVATE
+                $<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>,$<CONFIG:Coverage>>:${tpre_SANITIZER_FLAGS}>
+        )
+    endif()
 
     # set include directories
-    target_include_directories(${tpre_TARGET}
-        PUBLIC
-            ${tpre_PUBLIC_INCLUDE_DIRS}
-        PRIVATE
-            ${tpre_PRIVATE_INCLUDE_DIRS}
-    )
+    if(DEFINED tpre_PUBLIC_INCLUDE_DIRS AND (NOT ${interface_target}))
+        target_include_directories(${tpre_TARGET}
+            PUBLIC
+                ${tpre_PUBLIC_INCLUDE_DIRS}
+        )
+    endif()
+
+    if(DEFINED tpre_PRIVATE_INCLUDE_DIRS AND (NOT ${interface_target}))
+        target_include_directories(${tpre_TARGET}
+            PRIVATE
+                ${tpre_PRIVATE_INCLUDE_DIRS}
+        )
+    endif()
+
+    if(DEFINED tpre_INTERFACE_INCLUDE_DIRS)
+        target_include_directories(${tpre_TARGET}
+            INTERFACE
+                ${tpre_INTERFACE_INCLUDE_DIRS}
+        )
+    endif()
 
     # set compile definitions
-    target_compile_definitions(${tpre_TARGET}
-        PUBLIC
-            ${tpre_PUBLIC_DEFINITION_FLAGS}
-        PRIVATE
-            ${tpre_PRIVATE_DEFINITION_FLAGS}
-    )
+    if(DEFINED tpre_PUBLIC_DEFINITION_FLAGS AND (NOT ${interface_target}))
+        target_compile_definitions(${tpre_TARGET}
+            PUBLIC
+                ${tpre_PUBLIC_DEFINITION_FLAGS}
+        )
+    endif()
+
+    if(DEFINED tpre_PRIVATE_DEFINITION_FLAGS AND (NOT ${interface_target}))
+        target_compile_definitions(${tpre_TARGET}
+            PRIVATE
+                ${tpre_PRIVATE_DEFINITION_FLAGS}
+        )
+    endif()
+
+    if(DEFINED tpre_INTERFACE_DEFINITION_FLAGS)
+        target_compile_definitions(${tpre_TARGET}
+            INTERFACE
+                ${tpre_INTERFACE_DEFINITION_FLAGS}
+        )
+    endif()
 
     # this enables check for extraneous files when linking
     set_target_properties(${tpre_TARGET}
@@ -133,18 +190,33 @@ function(configure_target)
     )
 
     # set linker options for the target
-    target_link_options(${tpre_TARGET}
-        PRIVATE
-            ${tpre_LINKER_FLAGS}
-            $<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>,$<CONFIG:Coverage>>:${tpre_SANITIZER_FLAGS}>
-    )
+    if(DEFINED tpre_LINKER_FLAGS AND (NOT ${interface_target}))
+        target_link_options(${tpre_TARGET}
+            PRIVATE
+                ${tpre_LINKER_FLAGS}
+        )
+    endif()
+
+    if(DEFINED tpre_SANITIZER_FLAGS AND (NOT ${interface_target}))
+        target_link_options(${tpre_TARGET}
+            PRIVATE
+                $<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>,$<CONFIG:Coverage>>:${tpre_SANITIZER_FLAGS}>
+        )
+    endif()
 
     if(DEFINED tpre_C_STANDARD)
         # set C standard for the target
-        target_compile_features(${tpre_TARGET}
-            PUBLIC
-                c_std_${tpre_C_STANDARD}
-        )
+        if(NOT ${interface_target})
+            target_compile_features(${tpre_TARGET}
+                PUBLIC
+                    c_std_${tpre_C_STANDARD}
+            )
+        else()
+            target_compile_features(${tpre_TARGET}
+                INTERFACE
+                    c_std_${tpre_C_STANDARD}
+            )
+        endif()
 
         # disable C compiler extensions e.g. GNU
         set_target_properties(${tpre_TARGET}
@@ -155,10 +227,17 @@ function(configure_target)
 
     if(DEFINED tpre_CXX_STANDARD)
         # set C++ standard for the target
-        target_compile_features(${tpre_TARGET}
-            PUBLIC
-                cxx_std_${tpre_CXX_STANDARD}
-        )
+        if(NOT ${interface_target})
+            target_compile_features(${tpre_TARGET}
+                PUBLIC
+                    cxx_std_${tpre_CXX_STANDARD}
+            )
+        else()
+            target_compile_features(${tpre_TARGET}
+                INTERFACE
+                    cxx_std_${tpre_CXX_STANDARD}
+            )
+        endif()
 
         # disable C++ compiler extensions e.g. GNU
         set_target_properties(${tpre_TARGET}
@@ -167,7 +246,7 @@ function(configure_target)
         )
     endif()
 
-    if(tpre_ENABLE_UNUSED_SECTION_GARBAGE_COLLECTION)
+    if(tpre_ENABLE_UNUSED_SECTION_GARBAGE_COLLECTION AND (NOT ${interface_target}))
         if(("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
             OR ("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
             OR ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
